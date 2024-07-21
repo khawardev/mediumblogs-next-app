45b8557c807713938cf50972584605e6e4ab7b91
Khawar Sultan <khawarsultan.developer@gmail.com>
2024-07-21 12:44:31 +0500

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex-between  sm:py-[126px] py-[110px] ">
+    <div className="flex-between  sm:py-[126px] py-[106px] ">
       <div>
         <p className="lg:text-9xl md:text-8xl text-7xl  text-[#242424] leading-[0.9] tracking-tight">
           Human <br /> stories & ideas
